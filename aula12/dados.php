2a7bafdc04e6fa2bf04be344ebd613083c4fd964
arianegabriela <125364480+arianegabriela@users.noreply.github.com>
2023-04-04 07:40:15 -0300

    Add files via upload

---
+++ aula12/dados.php
@@ -0,0 +1,30 @@
+<?php
+
+$noticia1 = [ "titulo" => "Vídeo de drone mostra cidade do Maranhão que corre risco de desaparecer por causa de crateras",
+ "materia" => "As fortes chuvas que ocorrem nos últimos meses no Maranhão têm causado vários transtornos e assustado moradores. Buriticupu, cidade a 417 km de São Luís, vive em alerta por conta dos riscos causados pelas voçorocas, enormes crateras que, há mais de 30 anos, vem invadindo parte da cidade. Imagens de drone (veja acima) mostram que o município do Maranhão corre o risco de desaparecer por causa de crateras. São 26 buracos gigantes que avançam sobre a cidade e tiram o sono dos moradores.",
+ "foto" => "https://s2.glbimg.com/ny6oT-BXkd_I02KUtHoGAiw2aaY=/0x0:2820x1825/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/d/K/rd9DSxTrAyWGuA0lVn2w/dji-0468-2-.jpg",
+ "chamada" => "Buriticupu é a cidade maranhense com a maior concentração de crateras. São 26 buracos gigantes que avançam sobre a cidade e tiram o sono dos moradores"
+];
+
+$noticia2 = [
+ "titulo" => "Queda de raio mata adolescente de 16 anos em Chapadinha, no MA",
+ "materia" => "Na tarde deste sábado (25), um adolescente de 16 anos morreu após ser atingido por um raio durante uma partida de futebol 
+ no Povoado São Martins, na Zona Rural de Chapadinha, a cerca de 245 km de São Luís.",
+ "foto" => "https://s2.glbimg.com/-VzijwEurwzdk8CCSChaWX1j3dI=/0x0:1700x1065/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/M/3/NvBUxUSBAs7fFBUmnZ6w/imagem-g1-23-.png",
+ "chamada" => "Luís Louredo jogava bola em um campo com amigos quando foi atingido."
+];
+
+$noticia3 = [
+ "titulo" => "Assédio moral e assédio sexual: entenda como reconhecer agressões no ambiente de trabalho",
+ "materia" => "O assédio sexual é toda conduta de natureza sexual exercida sobre alguém sem seu consentimento e com restrição à sua liberdade de dizer não. São atos que atingem a honra, a dignidade e a moral da vítima.",
+ "foto" => "https://s2.glbimg.com/x9TNoa9K8Vh4TsQUlPPuhqi8Ka8=/0x0:620x465/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2015/06/16/bdms-assedio-moral.jpg",
+ "chamada" => "Cartilha publicada pelo Tribunal Superior do Trabalho orienta vítimas e testemunhas."
+];
+
+$noticia4 = [
+ "titulo" => "Prefeito de Petrópolis, Rubens Bomtempo é alvo de operação contra suposta fraude em licitações",
+ "materia" => "O prefeito de Petrópolis, Rubens Bomtempo (PSB), é alvo nesta quarta-feira (29) da Operação PII,    do Ministério Público do Rio de Janeiro (MPRJ) e da Polícia Civil do RJ, que investiga possível fraude em licitações     no município da Região Serrana fluminense.", 
+ "foto" => "https://s2.glbimg.com/L7rPPKnX_uglI9Q_8zXduwE5ZIY=/0x0:824x463/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/f/A/iNrHo4T1ac6atmUROWJQ/petropolis.jpg",
+ "chamada" => "Promotores cumpriram mandados na casa do prefeito e no gabinete dele. Força-tarefa também investiga empresários de ônibus."
+];
+
